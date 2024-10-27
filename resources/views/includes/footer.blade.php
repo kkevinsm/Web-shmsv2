b69f3ff9289de335a50bcf85a515b0611a9e9a47
kkevinsm <76790044+kkevinsm@users.noreply.github.com>
2024-10-27 13:37:58 +0700

    pertama kali

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,15 @@
+
+
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2024 &copy; SHMS V2</p>
+        </div>
+        <div class="float-end">
+            <p>Made with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by <a href="https:instagram.com/kkevinsm">Kev</a></p>
+            <!-- <p>Made with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by Kev</a></p> -->
+        </div>
+    </div>
+</footer>
